90e6fc4f6ad3043b61ad9fba499615a650247c82
AbhiramWarrier <kitkatmit1@gmail.com>
2025-06-13 21:05:37 +0200

    Add files via upload

---
+++ lesson 2/my info.py
@@ -0,0 +1,3 @@
+print("my name is Abhiram")
+print("my favorite color is black")
+print(":)")
---
+++ lesson 2/variable number.py
@@ -0,0 +1,6 @@
+fruit = "orange"
+number = 6
+print (fruit)
+print (number)
+animal = "monkey"
+print(animal)
