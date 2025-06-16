4580b6a7c0b9c389d82aad1a252424f4f3d5c8a5
AbhiramWarrier <kitkatmit1@gmail.com>
2025-06-16 21:14:18 +0200

    Add files via upload

---
+++ lesson 3/activity 1.py
@@ -0,0 +1,6 @@
+number = 10
+print (number)
+number2= 4.8
+print(number2)
+name = "Abhiram"
+print (name)
---
+++ lesson 3/type checker.py
@@ -0,0 +1,9 @@
+print(type(10))
+print(type(4.8))
+print(type("Python"))
+b = 486.5
+print(type(b))
+b = 4.8
+print(int(b))
+b = 400
+print(float(b))
