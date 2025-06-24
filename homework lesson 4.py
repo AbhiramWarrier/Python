a231e01372d68231e762f98ecb081b1a46dbb004
AbhiramWarrier <kitkatmit1@gmail.com>
2025-06-24 21:50:05 +0200

    Add files via upload

---
+++ homework lesson 4.py
@@ -0,0 +1,22 @@
+boy1 = height_cm = 178
+boy2 = height_cm = 188
+boy3 = height_cm = 165
+boy4 = height_cm = 183
+boy5 = height_cm = 160
+girl6 = height_cm = 150
+girl7 = height_cm = 187 
+girl8 = height_cm = 167
+
+
+sum = boy1+boy2+boy3+boy4+boy5
+girlsum = girl6+girl7+girl8
+print("the sum of all the 5 boys is:",sum)
+print("the sum of all the 3 girls is:",girlsum)
+
+
+
+
+average = sum/5
+print("the average of all the 5 boys is:", average)
+average = girlsum/3
+print("the average of all the 3 girls is:", average)
