4d3c1c3d7d23d248a8fe9a55f7425b4aaa7e740a
AbhiramWarrier <kitkatmit1@gmail.com>
2025-06-29 17:32:44 +0200

    Add files via upload

---
+++ suggest what to carry based on weather.py
@@ -0,0 +1,8 @@
+i= str(input("enter the kind of weather  "))
+if i== "sunny":
+    print ("suggest sunglasses")
+if(i == "rainy"): 
+    print("suggest umbrella")
+if(i == "snowy"):
+        print("wear a coat")
+
