f77ce27cff4f5ba0651a5983af825bfc56c7ef41
AbhiramWarrier <kitkatmit1@gmail.com>
2025-07-29 08:06:08 +0200

    Add files via upload

---
+++ homework condition statement.py
@@ -0,0 +1,10 @@
+medical_cause = input ("enter your body temprature")
+if ("you feel well enough"):
+ print ("you are allowed to travel")
+else:
+ print ("you are not allowed to travel")
+if ("your body temprature = < 37"):
+  print ("you are not allowed to travel")
+else:
+ print ("you are allowed to travel")
+ 
