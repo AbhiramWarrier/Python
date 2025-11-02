0daf22942ed3904404c64c278ce76d869f6c92ea
AbhiramWarrier <kitkatmit1@gmail.com>
2025-11-02 20:11:12 +0100

    Add files via upload

--- homework.py
+++ homework.py
@@ -1,12 +1,12 @@
-a = 10 
-print ("value of a:", a)
-print("type of a:", type(a))
-b = 3.14 
-print("value of b:", b)
-print("type of b:",type (b))
-c = "Hello"
-print ("value of c:", c)
-print ("type of c:", type (c))
-d = True
-print("value of d:",d)
-print("type of d:", type (d))
+import pygame
+pygame.init()
+SCREEN_WIDTH, SCREEN_HEIGHT = 500, 500
+
+display_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('my first game screen')
+background_image = pygame.transform.scale(pygame.image.load('background.png').convert(), (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+#tiger_image = pygame.transform.scale(pygame.image.load('hello tiger.png') .convert_alpha(), (300,300))
+backgrond_color = (58,158,158)
+display_surface.fill(backgrond_color)
+pygame.display.flip()
